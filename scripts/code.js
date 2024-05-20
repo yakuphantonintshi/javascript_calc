b5ecee25c5860bf8b5be791cecf3f6bee8529dc2
yakuphantonintshi <yakupha58756@gmail.com>
2024-05-20 09:37:18 +0200

    Add File

--- scripts/code.js
+++ scripts/code.js
@@ -1,18 +1,14 @@
-let buttons = document.querySelectorAll(".buttons, .operators")
-let screen = document.querySelector('.screen')
-let equal = document.querySelector('.equal')
-let clear = document.querySelector('.clear')
+let dataEntry = document.querySelector('[data-entry]')
 
-//functionality for numbers and operators
-buttons.forEach(button =>{
-    button.addEventListener('click' , () =>{
-        screen.value += button.innerHTML
-    })
-})
-//functionality for equal button
-equal.addEventListener('click' , () =>{
-    screen.value = eval(screen.value)
-})
-clear.addEventListener('click' , ()=>{
-    screen.value = ''
-})
+function display(value){
+   dataEntry.value += value
+}
+
+
+ document.querySelector('[equal]').addEventListener('click' , () =>{
+    let entry = dataEntry.value;
+    dataEntry.value = eval(entry)
+ })
+   document.querySelector('.clear').addEventListener('click', () => {
+      dataEntry.value = ''
+ })
